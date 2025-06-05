383b3fec699399abdf21f582d41e5dfbf28ae170
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-05 17:15:57 +0700

    Класс для механики управления игрой

---
+++ GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+
+namespace FlappyBird
+{
+  public class GameController
+  {
+    private GameStateManager stateManager;
+
+    public GameController(GameStateManager stateManager)
+    {
+      this.stateManager = stateManager;
+    }
+
+    public void ProcessKeyboardInput(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.Space)
+      {
+        switch (stateManager.CurrentState)
+        {
+          case GameStateManager.GameState.WaitingToStart:
+            stateManager.StartGame();
+            break;
+          case GameStateManager.GameState.Playing:
+            stateManager.Jump();
+            break;
+          case GameStateManager.GameState.GameOver:
+            stateManager.RestartGame();
+            break;
+        }
+      }
+    }
+  }
+}
